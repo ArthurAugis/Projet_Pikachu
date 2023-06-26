4d86f4c54fb8f032597efc51d1604254d8bb660b
RainbowYoshi <92651106+ArthurAugis@users.noreply.github.com>
2023-06-26 22:27:40 +0200

    Add files via upload

--- Site/Front/JS/index.js
+++ Site/Front/JS/index.js
@@ -1,31 +1,31 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const pokemonButtons = document.querySelectorAll(".pokemon");
-    const pokemonImg = document.querySelector("#pokemon-img");
-    let selectedPokemonImage = null;
+const pokemonButtons = document.querySelectorAll(".pokemon");
+const pokemonImg = document.querySelector("#pokemon-img");
+let selectedPokemonImage = null;
 
-    pokemonButtons.forEach(function (button, index) {
-        button.addEventListener("mouseover", function () {
-            const newSelectedPokemonImage = button.getAttribute("data-image");
-            if (selectedPokemonImage !== newSelectedPokemonImage) {
-                selectedPokemonImage = newSelectedPokemonImage;
-                pokemonImg.src = selectedPokemonImage;
+pokemonButtons.forEach(function (button, index) {
+    button.addEventListener("mouseover", function () {
+        const newSelectedPokemonImage = button.getAttribute("data-image");
+        if (selectedPokemonImage !== newSelectedPokemonImage) {
+            selectedPokemonImage = newSelectedPokemonImage;
+            pokemonImg.src = selectedPokemonImage;
 
-                pokemonButtons.forEach(function (otherButton) {
-                    if (otherButton !== button) {
-                        otherButton.classList.remove("selected");
-                    }
-                });
+            pokemonButtons.forEach(function (otherButton) {
+                if (otherButton !== button) {
+                    otherButton.classList.remove("selected");
+                }
+            });
 
-                button.classList.add("selected");
-            }
-        });
+            button.classList.add("selected");
+        }
     });
+});
 
-    function updatePokemonImage() {
-        pokemonImg.src = pokemonImages[selectedPokemonIndex];
+function updatePokemonImage() {
+    if (selectedPokemonImage !== null) {
+        pokemonImg.src = selectedPokemonIndex;
     }
+}
 
-    window.addEventListener("load", function () {
-        updatePokemonImage();
-    });
+window.addEventListener("load", function () {
+    updatePokemonImage();
 });
